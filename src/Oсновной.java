0405b89ebee2448e22eb823a5cf70003734dd9fa
konstanZX2 <zxgenerationzx@gmail.com>
2022-04-05 20:07:49 +0200

    я не знаю, Cделана вторая фиксация, сделано путем создания нового рефакторинга ветки

--- src/Oсновной.java
+++ src/Oсновной.java
@@ -1,5 +1,6 @@
 public class Oсновной {
     public static void main(String[] args) {
         System.out.println("Привет мир");
+        System.out.println("Изменение, внесенное путем создания филиала факторинга");
     }
 }
