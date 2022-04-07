5031211c433610d2930dd07b922840572e5b7a2e
konstanZX2 <zxgenerationzx@gmail.com>
2022-04-07 15:47:28 +0200

    Revert "я не знаю, Cделана вторая фиксация, сделано путем создания нового рефакторинга ветки"
    This reverts commit 36f0f305

--- src/Oсновной.java
+++ src/Oсновной.java
@@ -4,8 +4,6 @@ public class Oсновной {
         System.out.println("Изменение, внесенное путем создания филиала факторинга");
         System.out.println("Изменение сделано на github");
         System.out.println("Еще одно изменение, чтобы попробовать .gitignore");
-        System.out.println("изменение чтоб был конфликт с github");
         System.out.println("hui");
-        System.out.println("изменение чтобы увидеть откат");
     }
 }
